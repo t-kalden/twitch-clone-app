750a6caab0a7b9c84c201bf80f74d8b4ce7f30b1
Kalden <kalden.t@outlook.com>
2024-02-05 13:59:25 -0500

    added create,update and delete user functionality to create, update and delete user model in db

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,7 +1,8 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
- 
+import { db } from '@/lib/db'
+
 export async function POST(req: Request) {
  
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
@@ -48,11 +49,45 @@ export async function POST(req: Request) {
   }
  
   // Get the ID and type
-  const { id } = evt.data;
   const eventType = evt.type;
- 
-  console.log(`Webhook with and ID of ${ id } and type of ${ eventType }`)
-  console.log('Webhook body:', body)
+
+  if(eventType === 'user.created') {
+    await db.user.create({
+        data: {
+            externalUserId: payload.data.id, // passing clerk user id 
+            username: payload.data.username,
+            imageUrl: payload.data.image_url,
+        }
+    })
+  }
+
+  if(eventType === 'user.updated') {
+    // const currentUser = await db.user.findUnique({
+    //     where: {
+    //         externalUserId: payload.data.id
+    //     }
+    // })
+
+    // if(!currentUser) return new Response('User not found', { status : 404 })
+
+    await db.user.update({
+        where: {
+            externalUserId: payload.data.id,
+        },
+        data : {
+            username: payload.data.username,
+            imageUrl: payload.data.image_url
+        }
+    })
+  }
+
+  if(eventType === 'user.deleted') {
+    await db.user.delete ({
+        where: {
+            externalUserId: payload.data.id
+        }
+    })
+  }
  
   return new Response('', { status: 200 })
 }
