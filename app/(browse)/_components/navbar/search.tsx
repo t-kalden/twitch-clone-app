e7528a00285ef07d2b8550aec32108324b845f67
Kalden <kalden.t@outlook.com>
2024-02-06 13:57:18 -0500

    created navbar, navbarlogo components

---
+++ app/(browse)/_components/navbar/search.tsx
@@ -0,0 +1,47 @@
+"use client"
+import qs from 'query-string'
+import { useState } from 'react'
+import { X, SearchIcon } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
+export const Search = () => {
+    const router = useRouter()
+    const [ value, setValue ] = useState("")
+
+    const onSubmit =(e:React.FormEvent<HTMLFormElement>)  => {
+        e.preventDefault()
+
+        if(!value) return
+
+        const url = qs.stringifyUrl({
+            url:'/',
+            query: { term : value }
+        }, { skipEmptyString : true } )
+
+        router.push(url)
+    }
+    return (
+        <form 
+        onSubmit={ onSubmit }
+        className='relative w-full lg:w-[400px] flex items-center '>
+            <Input 
+            value={ value }
+            onChange={(e) => setValue(e.target.value)}
+            placeholder='search' 
+            className="rounded-r-none focus-visible:ring-0 
+            focus-visible:ring-transparent 
+            focus-visible:ring-offset-0"
+            />
+            <Button 
+            type='submit' 
+            size="sm" 
+            variant='secondary' 
+            className='rounded-l-none'>
+                <SearchIcon className='h-5 w-5 text-muted-foreground'/>
+            </Button>
+        </form>
+    )
+}
