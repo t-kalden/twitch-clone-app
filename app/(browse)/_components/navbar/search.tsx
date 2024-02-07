b952738325d87c63a641b190c6cf66448b244376
Kalden <kalden.t@outlook.com>
2024-02-07 13:00:49 -0500

    redirect search value to search url (not yet created)

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -17,7 +17,7 @@ export const Search = () => {
         if(!value) return
 
         const url = qs.stringifyUrl({
-            url:'/',
+            url:'/search',
             query: { term : value }
         }, { skipEmptyString : true } )
 
