e04332b895ef2bb323b4b34b93716cba49ab18ff
Kalden <kalden.t@outlook.com>
2024-02-07 17:22:07 -0500

    creat main content container

---
+++ app/(browse)/_components/container.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { cn } from "@/lib/utils"
+import { useSidebar } from "@/store/use-sidebar"
+
+interface ContainerProps {
+    children : React.ReactNode
+}
+
+export const Container = ({
+    children
+} : ContainerProps) => {
+
+    const {
+        collapsed, onCollapse, onExpand
+    } = useSidebar((state) => state)
+
+    return (
+        <div className={ cn(
+            "flex-1",
+            collapsed ? 'ml-[70px]' : 'ml-[70px] lg:ml-60'
+        )}>
+            { children }
+        </div>
+    )
+}
