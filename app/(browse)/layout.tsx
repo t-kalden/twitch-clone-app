17b6a8124b5dc3900b45d50d0cbda90c3012ab8e
Kalden <kalden.t@outlook.com>
2024-02-07 15:49:31 -0500

    import sidebar component

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "./_components/navbar"
+import { Sidebar } from "./_components/sidebar"
 
 const BrowseLayout = ({
     children,
@@ -7,6 +8,7 @@ const BrowseLayout = ({
         <>
             <Navbar />
             <div className="flex h-full pt-20">
+                <Sidebar />
                 { children } 
             </div>
         </>
