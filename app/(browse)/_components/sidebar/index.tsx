751f1891d693d61ca6931734db45959e23078ad1
Kalden <kalden.t@outlook.com>
2024-02-13 11:09:29 -0500

    create loading skeleton and import

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,5 +1,5 @@
 import { getRecommended } from "@/lib/recommended-service"
-import { Recommended } from "./recommended"
+import { Recommended, RecommendedSkeleton } from "./recommended"
 import { Toggle } from "./toggle"
 import { Wrapper } from "./wrapper"
 
@@ -14,5 +14,14 @@ export const Sidebar = async () => {
                 <Recommended data={recommended}/> 
             </div>
         </Wrapper>
+    )
+}
+
+export const SidebarSkeleton = () => {
+    return(
+        <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60
+        h-full bg-background border-r border-[#2d2e35 z=50"  >
+            <RecommendedSkeleton />
+        </aside>
     )
 }
