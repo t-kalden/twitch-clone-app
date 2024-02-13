0f8ab7a94b273c187b617d4ed858a638dd85db83
Kalden <kalden.t@outlook.com>
2024-02-13 11:08:34 -0500

    mock loading timeout

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -2,6 +2,8 @@ import { db } from "@/lib/db";
 import { getSelf } from "./auth-service";
 
 export const getRecommended = async () => {
+    await new Promise(res => setTimeout(res, 3000))
+    
     const users = await db.user.findMany({
         orderBy: {
             createdAt: "desc"
