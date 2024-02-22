adc1cd5f6af62ba3a0ac97a19ece9c78ef851f57
Kalden <kalden.t@outlook.com>
2024-02-22 13:34:30 -0500

    updated rec services to no show logged in user in recommended sidebar

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -2,13 +2,39 @@ import { db } from "@/lib/db";
 import { getSelf } from "./auth-service";
 
 export const getRecommended = async () => {
-    await new Promise(res => setTimeout(res, 3000))
-    
-    const users = await db.user.findMany({
-        orderBy: {
-            createdAt: "desc"
-        }
-    })
+    // await new Promise(res => setTimeout(res, 3000))
+
+    let userId
+
+    try {
+        // get self id if available (logged in)
+        const self = await getSelf()
+        userId = self.id
+    } catch {
+        userId = null
+    }
+
+    let users = []
+    if( userId ) {
+        // Check if userId is available (if logged in)
+        users = await db.user.findMany({
+            // if logged inm done show
+            where: {
+                NOT: {
+                    id: userId
+                }
+            },
+            orderBy: {
+                createdAt: 'desc'
+            }
+        })
+    } else {
+         users = await db.user.findMany({
+            orderBy: {
+                createdAt: "desc"
+            }
+        })
+    }
 
     return users
 }
