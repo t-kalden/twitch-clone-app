2d9662fd85a909039ae2c17ad395fb224e4a901a
Kalden <kalden.t@outlook.com>
2024-02-23 11:27:45 -0500

    import and add toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { ThemeProvider } from "@/components/theme-provider";
-
+import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -25,6 +25,7 @@ export default function RootLayout({
           attribute="class"
           forcedTheme="dark"
           storageKey="gamehub-theme">
+            <Toaster theme={'light'} position="bottom-center"/>
             {children}
           </ThemeProvider>
           </body>
