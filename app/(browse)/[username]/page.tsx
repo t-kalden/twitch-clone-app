75e9d14a18e06a42cef080336f1f89158713b6dd
Kalden <kalden.t@outlook.com>
2024-02-24 13:52:27 -0500

    check if user is blocked

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -2,6 +2,7 @@ import { isFollowingUser } from "@/lib/follow-service"
 import { getUserByUsername } from "@/lib/user-service"
 import { notFound } from "next/navigation"
 import { Actions } from "./_components/actions"
+import { isBlockedByUser } from "@/lib/block-service"
 
 interface UserPageProps {
     params: {
@@ -17,12 +18,18 @@ const UserPage = async ({ params } : UserPageProps) => {
     }
 
     const isFollowing = await isFollowingUser(user.id)
+    const isBlocked = await isBlockedByUser(user.id)
+
+    // if(isBlocked) {
+    //     notFound()
+    // }
 
     return (
         <div className="flex flex-col gap-y-4">
             <p>User: {user.username}</p>
             <p>User ID: {user.id}</p>
             <p>Following: {`${isFollowing}`} </p>
+            <p>Blocked : {`${isBlocked}`}</p>
             <Actions userId={ user.id } isFollowing= { isFollowing }/>
         </div>
     )
