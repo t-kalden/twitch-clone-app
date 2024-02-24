b5c19abfdab033931064742c674a0afbf558afac
Kalden <kalden.t@outlook.com>
2024-02-23 19:18:51 -0500

    adding onUnfollow handle function

--- actions/follow.ts
+++ actions/follow.ts
@@ -1,7 +1,8 @@
 'use server'
 
-import { followUser } from "@/lib/follow-service"
+import { followUser, unfollowUser } from "@/lib/follow-service"
 import { revalidatePath } from "next/cache"
+import { Tenali_Ramakrishna } from "next/font/google"
 
 export const onFollow = async (id:string) => {
 
@@ -17,5 +18,21 @@ export const onFollow = async (id:string) => {
         return followedUser
     } catch (error) {
         throw new Error("Internal Error")
+    }
+}
+
+export const onUnfollow = async (id: string) => {
+    try {
+        const unfollowedUser = await unfollowUser(id)
+
+        revalidatePath('/')
+
+        if(unfollowedUser) {
+            revalidatePath(`/${unfollowedUser.following.username}`)
+        }
+
+        return unfollowedUser
+    } catch (error) {
+        throw new Error('Internal Error')
     }
 }
