688048beff35c1ac9631a03f81dcb7f9b441f432
Kalden <kalden.t@outlook.com>
2024-02-24 13:36:14 -0500

    added handle block and unblock functions

--- app/(browse)/[username]/_components/actions.tsx
+++ app/(browse)/[username]/_components/actions.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button"
 import { onFollow, onUnfollow } from "@/actions/follow"
 import { useTransition } from "react"
 import { toast } from "sonner"
+import { onBlock, onUnblock } from "@/actions/block"
 
 interface ActionsProps {
     isFollowing : boolean
@@ -31,13 +32,38 @@ export const Actions = ( { isFollowing, userId } : ActionsProps ) => {
                 .catch(() => toast.error('Oops! Something went wrong :('))
         })
     }
+
+    const handleBlock = () => {
+        startTransition(() => {
+            onBlock(userId) 
+                .then((data) => toast.success(`${data.blocked.username} has been blocked. They can no longer access your page.`))
+                .catch(() => toast.error('Failed to block user.'))
+        })
+    }
+
+    const handleUnblock = () => {
+        startTransition(() => {
+            onUnblock(userId) 
+                .then((data) => toast.success(`${data.blocked.username} has been unblocked. They can now access your page.`))
+                .catch(() => toast.error('Failed to unblock user.'))
+        })
+    }
     return (
-        <Button 
-            variant= 'primary'
-            onClick= { isFollowing ? handleUnfollow : handleFollow } //checks if following to unfollow function else has follow function
-            disabled= {  isPending }
-        >
-            { isFollowing ? 'Unfollow' : 'Follow' }
-        </Button>
+        <>
+            <Button 
+                variant= 'primary'
+                onClick= { isFollowing ? handleUnfollow : handleFollow } //checks if following to unfollow function else has follow function
+                disabled= {  isPending }
+                >
+                { isFollowing ? 'Unfollow' : 'Follow' }
+            </Button>
+
+            <Button
+                onClick= { handleUnblock }
+                disabled= { isPending }
+            >
+                Block
+            </Button>
+        </>
     )
 }
