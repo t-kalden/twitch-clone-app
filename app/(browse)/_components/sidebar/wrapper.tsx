417c570a55e0be57b7bd1ebcf48793061c4036b4
Kalden <kalden.t@outlook.com>
2024-02-24 12:24:53 -0500

    imported and added following component and  skeleton

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -6,6 +6,7 @@ import { useState, useEffect } from "react";
 import { ToggleSkeleton } from "./toggle";
 import { RecommendedSkeleton } from "./recommended";
 import { useIsClient } from "usehooks-ts"; 
+import { FollowingSkeleton } from "./following";
 
 interface WrapperProps {
     children : React.ReactNode;
@@ -18,6 +19,7 @@ export const Wrapper = ({ children } : WrapperProps) => {
     if(!isClient) return (
         <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full  bg-background border-r border-#2d2e35 z-50">
             <ToggleSkeleton /> 
+            <FollowingSkeleton />
             <RecommendedSkeleton />
         </aside>
     )
