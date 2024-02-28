9e5b0f03b0f531eac3defaffc031dbe3f16bf534
Kalden <kalden.t@outlook.com>
2024-02-28 09:35:41 -0500

    creating and importing NavItemSKeleton for loading sidebar navitems

--- app/(dashboard)/u/[username]/_components/sidebar/nav-item.tsx
+++ app/(dashboard)/u/[username]/_components/sidebar/nav-item.tsx
@@ -5,6 +5,7 @@ import { useCreatorSidebar } from "@/store/use-creator-sidebar"
 import { LucideIcon } from "lucide-react"
 import { cn } from "@/lib/utils"
 import Link from "next/link"
+import { Skeleton } from "@/components/ui/skeleton"
 
 interface NavItemProps {
     icon : LucideIcon
@@ -39,5 +40,16 @@ export const NavItem = ({icon: Icon, label, href, isActive} : NavItemProps)  => 
                 </div>
             </Link>
         </Button>
+    )
+}
+
+export const NavItemSkeleton = () => {
+    return (
+        <li className="flex items-center gap-x-4 px-3 py-2">
+            <Skeleton className="min-h-[48px] min-w-[48px] rounded-md"/>
+            <div className="flex-1 hidden lg:block">
+                <Skeleton className="h-6"/>
+            </div>
+        </li>
     )
 }
