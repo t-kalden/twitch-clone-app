a0b75ad815e9f4e9b8b3956619d6432162c88c99
Kalden <kalden.t@outlook.com>
2024-02-28 20:47:44 -0500

    include stream as props to pass for username

--- lib/user-service.ts
+++ lib/user-service.ts
@@ -2,7 +2,8 @@ import { db } from "./db"
 
 export const getUserByUsername = async (username:string) => {
     const user = await db.user.findUnique({
-        where: { username }
+        where: { username },
+        include: { stream : true }
     })
     return user
 }
