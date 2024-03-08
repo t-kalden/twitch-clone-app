ba4eda3f0fb527ecfe87b92bfdbb9d3b7f5ce18e
Kalden <kalden.t@outlook.com>
2024-03-08 11:27:53 -0500

    create component and functionality  and import full screen control

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -1,8 +1,10 @@
 'use client'
 
 import { Participant, Track } from "livekit-client"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 import { useTracks } from "@livekit/components-react"
+import { FullscreenControl } from "./fullscreen-control"
+import { useEventListener } from "usehooks-ts"
 
 interface LiveVideoProps {
     participant: Participant
@@ -11,6 +13,7 @@ interface LiveVideoProps {
 export const LiveVideo = ({participant} : LiveVideoProps) => {
     const videoRef = useRef<HTMLVideoElement>(null)
     const wrapperRef = useRef<HTMLDivElement>(null)
+    const [ isFullscreen, setIsFullscreen ] = useState(false)
 
     useTracks([ Track.Source.Camera, Track.Source.Microphone ])
     .filter(
@@ -20,7 +23,21 @@ export const LiveVideo = ({participant} : LiveVideoProps) => {
         track.publication.track?.attach(videoRef.current)
     })
         
+    const toggleFullscreen = () => {
+        if(isFullscreen) {
+            document.exitFullscreen()
+        } else if (wrapperRef?.current) {
+            wrapperRef.current.requestFullscreen()
+        }
+    }
 
+    const handleFullscreenChange = () => {
+        const isCurrentlyFullscreen = document.fullscreenElement !== null
+        setIsFullscreen(isCurrentlyFullscreen)
+    }
+
+    useEventListener('fullscreenchange', handleFullscreenChange, wrapperRef)
+    
     return (
         <div 
         className="relative h-full flex"
@@ -30,6 +47,14 @@ export const LiveVideo = ({participant} : LiveVideoProps) => {
             ref={videoRef}
             width='100%'
             />
+            <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
+                <div className="absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from-neutral-900 px-4">
+                    <FullscreenControl 
+                    isFullscreen={isFullscreen} 
+                    onToggle={toggleFullscreen}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
