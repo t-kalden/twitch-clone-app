87c91108141c13ac63ddb671e4da6911f3d5f619
Kalden <kalden.t@outlook.com>
2024-03-08 15:33:19 -0500

    create chat form skeleton

--- components/stream-player/chatform.tsx
+++ components/stream-player/chatform.tsx
@@ -83,3 +83,15 @@ export const ChatForm = ({
         </form>
     )
 }
+
+export const ChatFormSkeleton = () => {
+    return (
+        <div className="flex-flex-col items-center gap-y-4 p-3">
+            <Skeleton className="w-full h-10"/>
+            <div className="flex items-center gap-x-2 ml-auto">
+                <Skeleton className="h-7 w-7" />
+                <Skeleton className="h-7 w-12" />
+            </div>
+        </div>
+    )
+}
