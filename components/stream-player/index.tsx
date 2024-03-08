a5c600002666412e6718771ab23d3cfe2e0e9ab7
Kalden <kalden.t@outlook.com>
2024-03-08 14:15:00 -0500

    imported chat component

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -4,6 +4,9 @@ import { Stream, User } from "@prisma/client"
 import { useViewerToken } from "@/hooks/use-viewer-token"
 import { LiveKitRoom } from '@livekit/components-react'
 import { Video } from "./video"
+import { useChatSidebar } from "@/store/chat-sidebar"
+import { cn } from "@/lib/utils"
+import { Chat } from "./chat"
 
 interface StreamPlayerProps { 
     user: User & { stream: Stream | null}
@@ -13,6 +16,8 @@ interface StreamPlayerProps {
 
 export const StreamPlayer = ({user, stream, isFollowing} : StreamPlayerProps) => {
     const { token, name, identity } = useViewerToken(user.id)
+
+    const { collapsed } = useChatSidebar((state) => state)
  
     if(!token || !name || !identity) {
         return (
@@ -26,13 +31,31 @@ export const StreamPlayer = ({user, stream, isFollowing} : StreamPlayerProps) =>
             <LiveKitRoom
                 token={token}
                 serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
-                className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full" 
+                className={cn("grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full",
+                collapsed && 'lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2'
+                )}
             > 
                 <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
                     <Video 
                     hostName={user.username}
                     hostIdentity={user.id}
                     />
+                </div>
+
+                <div className={cn(
+                    'col-span-1',
+                    collapsed && 'hidden'
+                )}>
+                    <Chat 
+                    viewerName={name}
+                    hostName={user.username}
+                    hostIdentity={user.id}
+                    isFollowing={isFollowing}
+                    isChatEnabled={stream.isChatEnabled}
+                    isChatDelayed={stream.isChatDelayed}
+                    isChatFollowersOnly={stream.isChatFollowersOnly}
+                    /> 
+
                 </div>
             </LiveKitRoom>
         </>
