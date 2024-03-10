2c766485f5c053056647680390c8739fc1ae0fab
Kalden <kalden.t@outlook.com>
2024-03-10 18:02:04 -0400

    create chat skeleton

--- components/stream-player/chat-list.tsx
+++ components/stream-player/chat-list.tsx
@@ -2,6 +2,7 @@
 
 import { ReceivedChatMessage } from "@livekit/components-react"
 import { ChatMessage } from "./chat-message"
+import { Skeleton } from "../ui/skeleton"
 
 interface ChatListProps {
     messages: ReceivedChatMessage[],
@@ -27,6 +28,14 @@ export const ChatList = ({messages, isHidden} : ChatListProps) => {
                 data={message}
                 />
             ))}
+        </div>
+    )
+}
+
+export const ChatSkeleton = () => {
+    return (
+        <div className="flex h-full items-center justify-center">
+            <Skeleton className="w-1/2 h-6" />
         </div>
     )
 }
