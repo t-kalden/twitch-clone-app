04e3e14a0c2e687f03573ff3d00ab0834e8a1f19
Kalden <kalden.t@outlook.com>
2024-03-10 22:28:32 -0400

    imported stream header component

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -8,7 +8,7 @@ import { useChatSidebar } from "@/store/chat-sidebar"
 import { cn } from "@/lib/utils"
 import { Chat, ChatSkeleton } from "./chat"
 import { ChatToggle } from "./chat-toggle"
-import { Header } from "./header"
+import { Header, HeaderSkeleton } from "./header"
 
 interface StreamPlayerProps { 
     user: User & { stream: Stream | null}
@@ -78,7 +78,7 @@ export const StreamPlayerSkeleton = () => {
         <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols0-3 xl:grid-cols-3 2xl:grid-cols-6 h-full">
             <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
                 <VideoSkeleton />
-                {/* TODO: Header Skeleton */}
+                <HeaderSkeleton />
             </div>
             <div className="col-span-1 bg-background">
                 <ChatSkeleton />
