8241afd4b256ffbb61bd10c590fc2d77d0bfa3cd
Kalden <kalden.t@outlook.com>
2024-03-13 19:45:47 -0400

    import Infocard component and pass stream info props to children

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -9,6 +9,7 @@ import { cn } from "@/lib/utils"
 import { Chat, ChatSkeleton } from "./chat"
 import { ChatToggle } from "./chat-toggle"
 import { Header, HeaderSkeleton } from "./header"
+import InfoCard from "./info-card"
 
 interface StreamPlayerProps { 
     user: User & { stream: Stream | null}
@@ -50,6 +51,12 @@ export const StreamPlayer = ({user, stream, isFollowing} : StreamPlayerProps) =>
                     imageUrl={user.imageUrl}
                     isFollowing={isFollowing}
                     name={stream.name}
+                    />
+                    <InfoCard 
+                    hostIdentity={user.id}
+                    viewerIdentity={identity}
+                    name={stream.name}
+                    thumbnailUrl={stream.thumbnailUrl}
                     />
                 </div>
 
