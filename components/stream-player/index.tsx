af8df91144313cfbe76b21aa2ea9c7ab32f40e17
Kalden <kalden.t@outlook.com>
2024-03-14 17:58:37 -0400

    import and pass in about card props

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -10,9 +10,13 @@ import { Chat, ChatSkeleton } from "./chat"
 import { ChatToggle } from "./chat-toggle"
 import { Header, HeaderSkeleton } from "./header"
 import InfoCard from "./info-card"
+import AboutCard from "./about-card"
 
 interface StreamPlayerProps { 
-    user: User & { stream: Stream | null}
+    user: User & { 
+        stream: Stream | null,
+        _count: { followedBy: number }
+    }
     stream: Stream
     isFollowing: boolean
 }
@@ -57,6 +61,13 @@ export const StreamPlayer = ({user, stream, isFollowing} : StreamPlayerProps) =>
                     viewerIdentity={identity}
                     name={stream.name}
                     thumbnailUrl={stream.thumbnailUrl}
+                    />
+                    <AboutCard 
+                    hostName={user.username}
+                    hostIdentity={user.id}
+                    viewerIdentity={identity}
+                    bio={user.bio}
+                    followedByCount={user._count.followedBy}
                     />
                 </div>
 
