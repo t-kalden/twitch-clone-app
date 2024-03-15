476fc0a1ad75a27e3438cd875a472a8bb5294180
Kalden <kalden.t@outlook.com>
2024-03-15 11:05:01 -0400

    updated block actions to kick guests from stream

--- actions/block.ts
+++ actions/block.ts
@@ -1,30 +1,43 @@
+"use server";
 
-"use server"
+import { revalidatePath } from "next/cache";
+import { RoomServiceClient } from "livekit-server-sdk";
+
+import { getSelf } from "@/lib/auth-service";
 import { blockUser, unblockUser } from "@/lib/block-service"
-import { revalidatePath } from "next/cache"
 
-export const onBlock = async (id: string) =>  {
-    // TODO: Adapt to disconnect from livestream
-    // TODO: Allow ability to kick the guest
+const roomService = new RoomServiceClient(
+  process.env.LIVEKIT_API_URL!,
+  process.env.LIVEKIT_API_KEY!,
+  process.env.LIVEKIT_API_SECRET!,
+);
 
-    const blockedUser = await blockUser(id)
-    
-    revalidatePath('/')
+export const onBlock = async (id: string) => {
+  const self = await getSelf();
 
-    if(blockedUser) {
-        revalidatePath(`/${blockedUser.blocked.username}`)
-    }
+  let blockedUser;
 
-    return blockedUser
-}
+  try {
+    blockedUser = await blockUser(id);
+  } catch {
+    // This means user is a guest
+  }
 
-export const onUnblock = async (id: string) =>  {
-    const unblockedUser = await unblockUser(id)
-    // revalidatePath('/')
-    if(unblockedUser) {
-        revalidatePath(`/${unblockedUser.blocked.username}`)
-    }
+  try {
+    await roomService.removeParticipant(self.id, id);
+  } catch {
+    // This means user is not in the room
+  }
 
-    return unblockedUser
-}
+  revalidatePath(`/u/${self.username}/community`);
 
+  return blockedUser;
+};
+
+export const onUnblock = async (id: string) => {
+  const self = await getSelf();
+  const unblockedUser = await unblockUser(id);
+
+  revalidatePath(`/u/${self.username}/community`);
+  return unblockedUser;
+};
