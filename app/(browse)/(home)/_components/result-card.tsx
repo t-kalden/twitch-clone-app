3785435b1d323c32b54825ad6e0c43b4a6a02185
Kalden <kalden.t@outlook.com>
2024-03-18 13:26:00 -0400

    creted recommended component cards

---
+++ app/(browse)/(home)/_components/result-card.tsx
@@ -0,0 +1,24 @@
+import { Stream, User } from "@prisma/client"
+import Link from "next/link"
+import Thumbnail from "./thumbnail"
+
+interface ResultCardProps {
+    data: Stream & { user: User }
+}
+
+export const ResultCard = ({data} : ResultCardProps) => {
+
+    return(
+        <Link href={`/${data.user.username}`}>
+            <div className="h-full w-full space-y-4">
+                <Thumbnail 
+                src={data.thumbnailUrl}
+                fallback={data.user.imageUrl}
+                isLive={data.isLive}
+                username={data.user.username}
+                />
+
+            </div>
+        </Link>
+    )
+}
---
+++ app/(browse)/(home)/_components/results.tsx
@@ -0,0 +1,39 @@
+"use server"
+
+import { Skeleton } from "@/components/ui/skeleton"
+import { getStreams } from "@/lib/feed-service"
+import { ResultCard } from "./result-card"
+
+export const Results = async () => {
+    const data = await getStreams()
+
+    return (
+        <div>
+            <h2 className="text-lg font-semibold mb-4">Recommended streams</h2>
+            {data.length === 0 && (
+                
+                    <div className="text-md text-muted-foreground">
+                        No streams found
+                    </div>
+                )
+            }
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+                {
+                    data.map((result) => (
+                        <ResultCard
+                        key={result.id}
+                        data={result}
+                        />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+
+export const ResultsSkeleton = () => {
+    <div className="h-full p-8 max-w-screen-2xl mx-auto">
+      <Skeleton /> 
+    </div>
+  }
